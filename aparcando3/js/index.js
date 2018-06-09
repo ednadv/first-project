b30c2980bff20d7a27e067ec3c793ce32160b17f
ednadv <edomingosv@gmail.com>
2018-06-09 17:10:56 +0200

    Erase

--- aparcando3/js/index.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var game = new Game('canvas');
-    game.start();
-
-    document.onkeydown = function(event) {
-        game.onKeyDown(event.keyCode);
-    }
-}    
